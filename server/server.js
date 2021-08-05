df4375d751688d731d3dbb13040e69e9005f281d
lkp25 <lkp25@o2.pl>
2021-08-05 19:35:33 +0200

    added check if server runs in production or dev mode to choose the corresponding URL for CORS

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
+//true or false production?
+const production = process.env.NODE_ENV === 'production'
+//if false, fallbeck to devserver:
+const clientURL = production ? "realsiteurl.com" : 'http://localhost:1234'
+
 const io = require('socket.io')(3000, 
     //options 
     {
         cors: {
             //add client url so it is not blocked
-            origin: 'http://localhost:1234'
+            origin: clientURL
         }
     }
 )
